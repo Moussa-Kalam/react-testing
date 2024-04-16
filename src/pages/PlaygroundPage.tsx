6f6d2635dad821f076dec5e2eba9d68515ba3f5c
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-04-16 23:30:01 +0200

    test: user interaction in searchBox component

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,7 @@
-import ExpandableText from '../components/ExpandableText';
+import SearchBox from '../components/SearchBox';
 
 const PlaygroundPage = () => {
-  return (
-    <ExpandableText text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae cupiditate pariatur vel maiores ex animi voluptatem necessitatibus quas. Minima, recusandae atque velit quaerat, harum illo qui voluptatibus tempore reprehenderit, modi tenetur totam distinctio eligendi. Similique ex esse aspernatur voluptate incidunt ut perferendis rem aliquam debitis voluptates alias odio animi velit libero, dolores quia odit temporibus commodi corporis necessitatibus inventore! Quo harum accusantium sed quia earum iusto nam quae cum! Ea cupiditate iusto hic aliquam, nobis ullam nesciunt iste perferendis quia laborum ipsam iure minus, molestiae tempore corrupti. Sapiente ullam repellat, delectus fugit sed reprehenderit praesentium. Eveniet officiis repellat unde in!' />
-  );
+  return <SearchBox onChange={(text) => console.log(text)} />;
 };
 
 export default PlaygroundPage;
