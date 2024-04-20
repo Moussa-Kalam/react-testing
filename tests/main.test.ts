0a789e577a9ec7eda0637ebd028fbec7537c6f91
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-04-20 21:59:04 +0200

    feat: mock data with msw and faker

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,10 +1,9 @@
-import { it, expect, describe } from 'vitest';
+import { describe, it } from 'vitest';
+import { db } from './mocks/db';
 
 describe('group', () => {
-  it('should', async () => {
-    const response = await fetch('/categories');
-    const data = await response.json();
-    console.log(data);
-    expect(data).toHaveLength(3);
+  it('should', () => {
+    const product = db.product.create({ name: 'Apple' });
+    console.log(product);
   });
 });
