ce357906c197bb7b1acbe5c502b9b987b839ed93
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-04-20 22:22:55 +0200

    test: handle errors

--- tests/components/Productlist.test.tsx
+++ tests/components/Productlist.test.tsx
@@ -33,4 +33,12 @@ describe('ProductList', () => {
     const message = await screen.findByText(/no products/i);
     expect(message).toBeInTheDocument();
   });
+
+  it('should render an error message when there is an error', async () => {
+    server.use(http.get('/products', () => HttpResponse.error()));
+
+    render(<ProductList />);
+
+    expect(await screen.findByText(/error/i)).toBeInTheDocument();
+  });
 });
