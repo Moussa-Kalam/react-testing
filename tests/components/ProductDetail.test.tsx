53c13fd75b45688d873cdc40dcf5c4eea64e96a9
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-04-20 22:17:57 +0200

    test: fix error by testing mock data

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,18 +1,33 @@
 import { render, screen } from '@testing-library/react';
-import ProductDetail from '../../src/components/ProductDetail';
-import { products } from '../mocks/data';
 import { http, HttpResponse } from 'msw';
+import ProductDetail from '../../src/components/ProductDetail';
+import { db } from '../mocks/db';
 import { server } from '../mocks/server';
 
 describe('ProductDetail', () => {
+  let productId: number;
+
+  beforeAll(() => {
+    const product = db.product.create();
+    productId = product.id;
+  });
+
+  afterAll(() => {
+    db.product.delete({ where: { id: { equals: productId } } });
+  });
+
   it('should render the details of a product', async () => {
-    render(<ProductDetail productId={2} />);
+    const product = db.product.findFirst({
+      where: { id: { equals: productId } },
+    });
+
+    render(<ProductDetail productId={productId} />);
 
     expect(
-      await screen.findByText(new RegExp(products[1].name))
+      await screen.findByText(new RegExp(product!.name))
     ).toBeInTheDocument();
     expect(
-      await screen.findByText(new RegExp(products[1].price.toString()))
+      await screen.findByText(new RegExp(product!.price.toString()))
     ).toBeInTheDocument();
   });
 
